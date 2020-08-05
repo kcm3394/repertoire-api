245b44487a4791a24899b0c38dd2bc0c943792c4
Kelsey <makacakeee@gmail.com>
2020-08-05 12:22:38 -0400

    Created framework

---
+++ src/main/java/personal/kcm3394/repertoireapi/RepertoireApiApplication.java
@@ -0,0 +1,13 @@
+package personal.kcm3394.repertoireapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepertoireApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepertoireApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/personal/kcm3394/repertoireapi/RepertoireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package personal.kcm3394.repertoireapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepertoireApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
